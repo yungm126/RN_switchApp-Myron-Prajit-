a2d618bf8ebcb92c84c06ed6e0f0240f6d430d6e
XD-DISTRO <74779316+XD-DISTRO@users.noreply.github.com>
2025-09-17 07:41:10 -0500

    added switcher

---
+++ switchApp/app/components/Switcher.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Switcher = () => {
+  return (
+    <View>
+      <Text>Switcher</Text>
+    </View>
+  )
+}
+
+export default Switcher
+
+const styles = StyleSheet.create({})
